d1b59b6b8ffc65c1c4b2dcb34e08cfd0eae5fbe1
finnharbeke <code@finnharbeke.ch>
2026-02-02 21:29:37 +0100

    loading screen

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
+const NOTE_TIME = 2000;
+const SUCCESS_TIME = 1000;
+const COOLDOWN = 5000;
 
 let guilty: boolean | null = null;
-let wait: boolean = false;
 
 document.querySelector<HTMLButtonElement>('#coupable')!.onclick = () => {
   guilty = true;
@@ -8,35 +10,42 @@ document.querySelector<HTMLButtonElement>('#coupable')!.onclick = () => {
 document.querySelector<HTMLButtonElement>('#non-coupable')!.onclick = () => {
   guilty = false;
 };
-document.querySelector<HTMLButtonElement>('#vote')!.onclick = () => {
+
+document.querySelector<HTMLButtonElement>('#judge')!.onclick = () => {
+  let note = document.querySelector<HTMLParagraphElement>('#note')!;
   console.log(guilty);
-  if (wait) {
-    document.querySelector<HTMLButtonElement>('#note')!.innerHTML = `
-      Attendez svp...
-    `
-    return;
-  } else if (guilty === null) {
-    document.querySelector<HTMLButtonElement>('#note')!.innerHTML = `
+  if (guilty === null) {
+    note.innerHTML = `
       Vous avez pas encore fait une décision!
-    `
-    return;
-  } else {
-    if (guilty) {
-      document.querySelector<HTMLButtonElement>('#note')!.innerHTML = `
-      Vous avez votez <span class="text-rose-300">coupable</span>!
-      `
-    } else {
-      document.querySelector<HTMLButtonElement>('#note')!.innerHTML = `
-        Vous avez votez <span class="text-emerald-300">non coupable</span>!
-      `
-    }
-    wait = true;
+    `;
     setTimeout(() => {
-      wait = false;
-    }, 15000)
+      note.innerHTML = ''
+    }, NOTE_TIME);
+    return;
+  }
+  let button = document.querySelector<HTMLParagraphElement>('#judge')!;
+  button.setAttribute("disabled", "true");
+  if (guilty) {
+    note.innerHTML = `
+    Vous avez votez <span class="text-rose-300">coupable</span>!
+    `;
+  } else {
+    note.innerHTML = `
+      Vous avez votez <span class="text-emerald-300">non coupable</span>!
+    `;
   }
   guilty = null;
   setTimeout(() => {
-    document.querySelector<HTMLButtonElement>('#note')!.innerHTML = ''
-  }, 2000)
+    button.removeAttribute("disabled");
+    note.innerHTML = '';
+    let loader = document.querySelector<HTMLDivElement>('#loader')!;
+    loader.classList.remove("hidden");
+    setTimeout(() => {
+      loader.classList.add("hidden");
+    }, COOLDOWN)
+  }, SUCCESS_TIME);
+    // wait = true;
+    // setTimeout(() => {
+    //   wait = false;
+    // }, 15000)
 };
