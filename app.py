8cb5bd2b3ce1f9dc7dc7cce7bf575e1f6e8d3585
finnharbeke <code@finnharbeke.ch>
2026-02-04 11:41:17 +0100

    config window and first backend

---
+++ app.py
@@ -0,0 +1,80 @@
+import datetime as dt
+import json
+import sqlite3
+from http import HTTPStatus
+
+from flask import Flask, request, jsonify, make_response
+
+app = Flask(__name__)
+
+conn = sqlite3.connect('data.db', check_same_thread=False)
+conn.execute(
+    """
+    CREATE TABLE IF NOT EXISTS round
+    (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        desc TEXT NOT NULL
+    );""")
+conn.execute("""
+    CREATE TABLE IF NOT EXISTS vote
+    (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        round_id INTEGER NOT NULL,
+        guilty INTEGER NOT NULL CHECK(guilty >= 0 AND guilty <= 1),
+        datetime TEXT NOT NULL,
+        FOREIGN KEY(round_id) REFERENCES round(id)
+    );
+    """
+)
+conn.execute("""
+    INSERT OR REPLACE INTO round (id, desc) VALUES (1, 'test');
+""")
+conn.commit()
+
+# https://stackoverflow.com/questions/25594893/how-to-enable-cors-in-flask
+def _corsify(response):
+    response.headers.add("Access-Control-Allow-Origin", "http://localhost:5173")
+    return response
+
+def _build_cors_preflight_response():
+    response = make_response()
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add('Access-Control-Allow-Headers', "*")
+    response.headers.add('Access-Control-Allow-Methods', "*")
+    return response
+
+@app.route("/rounds")
+def rounds():
+    SELECT_ROUNDS = "SELECT * FROM round;"
+    rounds = conn.execute(SELECT_ROUNDS).fetchall()
+    obj = []
+    for id_, desc in rounds:
+        obj.append(dict(id=id_, desc=desc))
+    return _corsify(jsonify(obj))
+
+@app.route("/create-round", methods=["POST", "OPTIONS"])
+def create_round():
+    if request.method == "OPTIONS":
+        return _build_cors_preflight_response()
+    data = json.loads(request.data.decode())
+    if "description" not in data:
+        return "missing `description` field in request", HTTPStatus.BAD_REQUEST
+    description = data["description"]
+
+    INSERT_ROUND = """INSERT INTO round (desc) VALUES (?);"""
+    conn.execute(INSERT_ROUND, (description, ))
+    conn.commit()
+    return _corsify(make_response("created", HTTPStatus.CREATED))
+
+INSERT_VOTE = """INSERT INTO vote (round_id, guilty, datetime) VALUES (?, ?, ?);"""
+
+@app.route("/coupable", methods=["POST"])
+def coupable():
+    data = json.loads(request.data.decode())
+    if "round_id" not in data:
+        return "missing `round_id` field in request", HTTPStatus.BAD_REQUEST
+    round_id = data["round_id"]
+
+    conn.execute(INSERT_VOTE, (round_id, 1, dt.datetime.now().isoformat()))
+    conn.commit()
+    return "created", HTTPStatus.CREATED
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ const SUCCESS_TIME = 1000;
 const COOLDOWN = 5000;
 
 let guilty: boolean | null = null;
+let config_open = false;
 
 document.querySelector<HTMLButtonElement>('#coupable')!.onclick = () => {
   guilty = true;
@@ -10,6 +11,72 @@ document.querySelector<HTMLButtonElement>('#coupable')!.onclick = () => {
 document.querySelector<HTMLButtonElement>('#non-coupable')!.onclick = () => {
   guilty = false;
 };
+
+const fill_select = () => fetch("http://127.0.0.1:5000/rounds")
+  .then((data) => data.json())
+  .then((rounds: {id: number, desc: string}[]) => {
+    let select = document.querySelector<HTMLSelectElement>('#round-select')!;
+    select.innerHTML = '';
+    for (let i in rounds) {
+      let opt = document.createElement('option');
+      opt.value = rounds[i].id.toString();
+      opt.innerHTML = rounds[i].desc;
+      if (i == (rounds.length - 1).toString())
+        opt.setAttribute("selected", "true");
+      select.appendChild(opt);
+    }
+  })
+fill_select();
+
+// CONFIG WINDOW
+document.addEventListener("keydown", (e) => {
+  if (e.key != 'k' || !e.metaKey)
+    return;
+  if (config_open)
+    document.querySelector<HTMLDivElement>('#config')?.classList.add("hidden");
+  else
+    document.querySelector<HTMLDivElement>('#config')?.classList.remove("hidden");
+  config_open = !config_open;
+})
+
+document.querySelector<HTMLButtonElement>('#newround_button')!.onclick = () => {
+  let input = document.querySelector<HTMLInputElement>('#newround_input')!;
+  let description = input.value;
+  let note = document.querySelector<HTMLParagraphElement>('#newround_note')!;
+  const clear_note = () => setTimeout(() => {note.innerText = ''}, 2000);
+  if (description == "") {
+    note.classList.remove("text-green-400");
+    note.classList.add("text-red-400");
+    note.innerText = 'cannot use an empty round name'
+    clear_note();
+    return;
+  }
+
+  fetch("http://127.0.0.1:5000/create-round", {
+    method: "post",
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        description,
+    })
+  }).then(() => {
+      note.classList.remove("text-red-400");
+      note.classList.add("text-green-400");
+      note.innerText = 'created round '+ description + '!';
+      input.value = '';
+      fill_select();
+  })
+  .catch(() => {
+    note.classList.remove("text-green-400");
+    note.classList.add("text-red-400");
+    note.innerText = "error: couldn't create round";
+  })
+  .finally(clear_note)
+
+}
+
 
 document.querySelector<HTMLButtonElement>('#judge')!.onclick = () => {
   let note = document.querySelector<HTMLParagraphElement>('#note')!;
